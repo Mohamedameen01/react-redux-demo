e124fc366c2517944b7ba54942382d825eb14e08
mohamedameen01 <ameentp64@gmail.com>
2023-12-13 21:15:49 +0530

    Applied Middleware
    redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
-// To bind the reducer in the store.
-import { legacy_createStore } from "redux";
-
+// Importing createStore to bind the reducer in the store.
+// Importing applymiddleware.
+import { legacy_createStore, applyMiddleware } from "redux";
+// Importing Logger.
+import logger from "redux-logger";
 import rootReducer from "./rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-
-
-const store = legacy_createStore(rootReducer);
+// Passing applyMiddleware as an argument of createStore.
+// And passing logger as an argument of applyMiddleware to get information that - 
+// related to redux store.
+const store = legacy_createStore(rootReducer, composeWithDevTools(
+    applyMiddleware(logger)));
 
 export default store
